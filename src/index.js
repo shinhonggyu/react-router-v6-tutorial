e66076de08b870262fa75d44323f45fded64cf47
shinhonggyu <zowoz0@naver.com>
2022-03-18 16:26:38 +0900

    restart

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { render } from "react-dom";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+const App = () => {
+  return <h1>자살하기</h1>;
+};
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
